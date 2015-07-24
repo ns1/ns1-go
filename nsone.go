11b9b132dca0857e9d26da353b079410bb0297ca
Tomas Doran <tdoran@yelp.com>
2015-07-24 20:52:21 +0100

    Add secondary details about zones, so we can slave

--- nsone.go
+++ nsone.go
@@ -19,21 +19,31 @@ type ZonePrimary struct {
 	Secondaries []string `json:"secondaries,omitempty"`
 }
 
+type ZoneSecondary struct {
+	Status       string `json:"status,omitempty"`
+	Last_xfr     int    `json:"last_xfr,omitempty"`
+	Primary_ip   string `json:"primary_ip,omitempty"`
+	Primary_port int    `json:"primary_port,omitempty"`
+	Enabled      bool   `json:"enabled"`
+	Expired      bool   `json:"expired,omitempty"`
+}
+
 type Zone struct {
-	Id      string `json:"id,omitempty"`
-	Ttl     int    `json:"ttl,omitempty"`
-	Nx_ttl  int    `json:"nx_ttl,omitempty"`
-	Retry   int    `json:"retry,omitempty"`
-	Zone    string `json:"zone,omitempty"`
-	Refresh int    `json:"refresh,omitempty"`
-	Expiry  int    `json:"expiry,omitempty"`
-	//	Primary       ZonePrimary       `json:"primary,omitempty"`
+	Id            string            `json:"id,omitempty"`
+	Ttl           int               `json:"ttl,omitempty"`
+	Nx_ttl        int               `json:"nx_ttl,omitempty"`
+	Retry         int               `json:"retry,omitempty"`
+	Zone          string            `json:"zone,omitempty"`
+	Refresh       int               `json:"refresh,omitempty"`
+	Expiry        int               `json:"expiry,omitempty"`
+	Primary       ZonePrimary       `json:"primary,omitempty"`
 	Dns_servers   []string          `json:"dns_servers,omitempty"`
 	Networks      []int             `json:"networks,omitempty"`
 	Network_pools []string          `json:"network_pools,omitempty"`
 	Hostmaster    string            `json:"hostmaster,omitempty"`
 	Pool          string            `json:"pool,omitempty"`
 	Meta          map[string]string `json:"meta,omitempty"`
+	Secondary     ZoneSecondary     `json:"secondary,omitempty"`
 }
 
 type Answer struct {
