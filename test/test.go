38261a4f498d4e535723e38e6c4c51cf3b7610f9
Tomas Doran <tdoran@yelp.com>
2015-07-24 02:17:57 +0100

    More example fixes

--- test/test.go
+++ test/test.go
@@ -3,20 +3,22 @@ package main
 import (
 	"fmt"
 	"github.com/bobtfish/go-nsone-api"
+	"os"
 )
 
 func main() {
-	if k := os.Getenv("NSONE_APIKEY"); k == "" {
+	k := os.Getenv("NSONE_APIKEY")
+	if k == "" {
 		fmt.Println("NSONE_APIKEY environment variable is not set, giving up")
 	}
-	apiu := nsone.New(k)
-	fmt.Println(n.GetZones())
+	api := nsone.New(k)
+	fmt.Println(api.GetZones())
 
 	z := nsone.NewZone("foo.com")
 	api.CreateZone(z)
 
 	r := nsone.NewRecord("foo.com", "www.foo.com", "A")
-	r.Answers = []nsone.Answer{nsone.Answer{Answer: "1.1.1.1"}}
+	r.Answers = []nsone.Answer{nsone.Answer{Answer: []string{"1.1.1.1"}}}
 	api.CreateRecord(r)
 
 	api.DeleteZone("foo.com")
