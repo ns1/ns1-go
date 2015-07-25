69bf9cd3708bc12ad68e2af130eaf83ece272fd4
Tomas Doran <tdoran@yelp.com>
2015-07-25 21:11:31 +0100

    Add the config field (and others) to feeds

--- datasource.go
+++ datasource.go
@@ -1,9 +1,18 @@
 package nsone
 
+type FeedDestination struct {
+	Destid   string `json"destif"`
+	Desttype string `json"desttype"`
+	Record   string `json"record"`
+}
+
 type DataSource struct {
-	Id         string `json:"id,omitempty"`
-	Name       string `json:"name"`
-	SourceType string `json"source_type"`
+	Id           string            `json:"id,omitempty"`
+	Name         string            `json:"name"`
+	SourceType   string            `json"source_type"`
+	Config       map[string]string `json"config,omitempty"`
+	Status       string            `json"status,omitempty"`
+	Destinations []FeedDestination `json"destinations,omitempty"`
 }
 
 func NewDataSource(name string, source_type string) *DataSource {
