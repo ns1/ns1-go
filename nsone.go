f8253f7e300e65e9ebaedcf0e1797817bbebf24b
Tomas Doran <tdoran@yelp.com>
2015-07-30 21:49:42 +0100

    Get QPS stats

--- nsone.go
+++ nsone.go
@@ -196,3 +196,16 @@ func (c APIClient) DeleteMonitoringJob(id string) error {
 func (c APIClient) UpdateMonitoringJob(mj *MonitoringJob) error {
 	return c.doHTTPBoth("POST", fmt.Sprintf("https://api.nsone.net/v1/monitoring/jobs/%s", mj.Id), mj)
 }
+
+func (c APIClient) GetQPSStats() (v float64, err error) {
+	var s map[string]float64
+	_, err = c.doHTTPUnmarshal("GET", "https://api.nsone.net/v1/stats/qps", nil, &s)
+	if err != nil {
+		return v, err
+	}
+	v, found := s["qps"]
+	if !found {
+		return v, errors.New("Could not find 'qps' key in returned data")
+	}
+	return v, nil
+}
