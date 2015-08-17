d0e955013705422d55c7229c7aaf23aebd983590
Tomas Doran <tdoran@yelp.com>
2015-08-17 11:21:29 +0100

    Omit the API key when creating

--- apikey.go
+++ apikey.go
@@ -3,7 +3,7 @@ package nsone
 type Apikey struct {
 	Id          string         `json:"id,omitempty"`
 	Name        string         `json:"name"`
-	Key         string         `json:"key"`
+	Key         string         `json:"key,omitempty"`
 	LastAccess  int            `json:"last_access,omitempty"`
 	Teams       []string       `json:"teams"`
 	Permissions PermissionsMap `json:"permissions"`
