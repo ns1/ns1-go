048ea027f3c0e355fbf56af225ba3eac0dc66be8
Tomas Doran <tdoran@yelp.com>
2015-10-06 01:57:57 -0700

    This is the string value '1', gross

--- monitoring.go
+++ monitoring.go
@@ -32,7 +32,7 @@ type MonitoringJob struct {
 	RegionScope    string                         `json:"region_scope"`
 	Notes          string                         `json:"notes,omitempty"`
 	Name           string                         `json:"name"`
-	Ssl            int                            `json:"ssl,omitempty"`
+	Ssl            string                         `json:"ssl,omitempty"`
 	NotifyRepeat   int                            `json:"notify_repeat"`
 	RapidRecheck   bool                           `json:"rapid_recheck"`
 	NotifyDelay    int                            `json:"notify_delay"`
