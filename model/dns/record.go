b26f1375dd98853c36f4e2547d1c2727eb65d998
Pasha Palangpour <ppalangpour@nsone.net>
2016-09-05 11:35:30 -0400

    Adds some Answer constructors for specific rr types.

--- model/dns/record.go
+++ model/dns/record.go
@@ -2,22 +2,23 @@ package dns
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
 // Record wraps an NS1 /zone/{zone}/{domain}/{type} resource
 type Record struct {
 	ID              string            `json:"id,omitempty"`
-	Zone            string            `json:"zone,omitempty"`
-	Domain          string            `json:"domain,omitempty"`
-	Type            string            `json:"type,omitempty"`
+	Zone            string            `json:"zone"`
+	Domain          string            `json:"domain"`
+	Type            string            `json:"type"`
 	Link            string            `json:"link,omitempty"`
-	Meta            map[string]string `json:"meta,omitempty"`
-	Answers         []*Answer         `json:"answers"`
-	Filters         []*Filter         `json:"filters,omitempty"`
 	TTL             int               `json:"ttl,omitempty"`
-	UseClientSubnet bool              `json:"use_client_subnet"`
+	UseClientSubnet bool              `json:"use_client_subnet,omitempty"`
+	Answers         []*Answer         `json:"answers,omitempty"`
+	Filters         []*Filter         `json:"filters,omitempty"`
 	Regions         map[string]Region `json:"regions,omitempty"`
+	Meta            map[string]string `json:"meta,omitempty"`
 }
 
 // Implementation of Stringer interface. Concatenates domain and record type.
@@ -26,15 +27,17 @@ func (r Record) String() string {
 }
 
 // NewRecord takes a zone, domain and record type t and creates a *Record with UseClientSubnet: true & empty Answers
+//
+// Answers are required on PUT, but not POST.
 func NewRecord(zone string, domain string, t string) *Record {
 	if !strings.HasSuffix(domain, zone) {
 		domain = fmt.Sprintf("%s.%s", domain, zone)
 	}
 	return &Record{
-		Zone:            zone,
-		Domain:          domain,
-		Type:            t,
-		UseClientSubnet: true,
+		Zone:    zone,
+		Domain:  domain,
+		Type:    t,
+		Answers: []*Answer{},
 	}
 }
 
@@ -42,4 +45,57 @@ func NewRecord(zone string, domain string, t string) *Record {
 func (r *Record) LinkTo(to string) {
 	r.Meta = nil
 	r.Link = to
+}
+
+func (r *Record) AddAnswer(ans *Answer) {
+	if r.Answers == nil {
+		r.Answers = []*Answer{}
+	}
+
+	r.Answers = append(r.Answers, ans)
+}
+
+// Answer wraps the values of a Record's "filters" attribute
+type Answer struct {
+	Rdata  []string               `json:"answer"`
+	Region string                 `json:"region,omitempty"`
+	Meta   map[string]interface{} `json:"meta,omitempty"`
+}
+
+// NewAnswer creates a generic Answer with given rdata.
+func NewAnswer(rdata []string) *Answer {
+	return &Answer{Rdata: rdata}
+}
+
+func NewAv4Answer(host string) *Answer {
+	return &Answer{Rdata: []string{host}}
+}
+
+func NewAv6Answer(host string) *Answer {
+	return &Answer{Rdata: []string{host}}
+}
+
+func NewALIASAnswer(host string) *Answer {
+	return &Answer{Rdata: []string{host}}
+}
+
+func NewCNAMEAnswer(name string) *Answer {
+	return &Answer{Rdata: []string{name}}
+}
+
+func NewTXTAnswer(text string) *Answer {
+	return &Answer{Rdata: []string{text}}
+}
+
+func NewMXAnswer(pri int, host string) *Answer {
+	return &Answer{Rdata: []string{strconv.Itoa(pri), host}}
+}
+
+func NewSRVAnswer(priority, weight, port int, target string) *Answer {
+	return &Answer{Rdata: []string{
+		strconv.Itoa(priority),
+		strconv.Itoa(weight),
+		strconv.Itoa(port),
+		target,
+	}}
 }
