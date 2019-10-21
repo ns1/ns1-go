c05de6c35d22dea074b8f124aed9a74fed45104f
rupa deadwyler <rdeadwyler@ns1.com>
2019-10-21 17:56:07 -0400

    WIP INT-3: refactor `mockAPI`
    yeah it's pretty dumb, but prefer to generalize/make it better when
    actually writing more tests that want to use it.

--- rest/zone_test.go
+++ rest/zone_test.go
@@ -80,82 +80,83 @@ func TestPagination_disabled(t *testing.T) {
 	}
 }
 
-const zoneListPath = "/zones"
-const zoneListQuery = "?after=coolzone.cool&limit=2"
-var zoneListLinkHeader = fmt.Sprintf(`</zones?%s>; rel="next"`, zoneListQuery)
-const zoneListPageOne = `[
-  {"zone": "example.com"},
-  {"zone": "coolzone.cool"}
-]`
-const zoneListPageTwo = `[
-  {"zone": "example.org"},
-  {"zone": "example.io"}
-]`
-
-const zoneGetPath = "/zones/coolzone.cool"
-const zoneGetQuery = "after=b.coolzone.cool&limit=2"
-var zoneGetLinkHeader = fmt.Sprintf(
-	`</zones/coolzone.cool?%s>; rel="next"`, zoneGetQuery,
-)
-const zoneGetPageOne = `{
-  "zone": "coolzone.cool",
-  "records": [{
-    "domain":"a.coolzone.cool",
-    "short_answers": ["1.1.1.1"],
-    "type":"A"
-  },
-  {
-    "domain":"b.coolzone.cool",
-    "short_answers": ["2.2.2.2"],
-    "type":"A"
-  }]
-}`
-const zoneGetPageTwo = `{
-  "zone": "coolzone.cool",
-  "records": [{
-    "domain":"c.coolzone.cool",
-    "short_answers": ["3.3.3.3"],
-    "type":"A"
-  },
-  {
-    "domain":"d.coolzone.cool",
-    "short_answers": ["4.4.4.4"],
-    "type":"A"
-  }]
-}`
-
 func mockAPI(t *testing.T) *httptest.Server {
-	return httptest.NewServer(
-		http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-			var body string
-			switch path := req.URL.Path; path {
 
-				case zoneListPath:
-					if req.URL.RawQuery == "" {
-						res.Header().Set("Link", zoneListLinkHeader)
-						body = zoneListPageOne
-					} else if req.URL.RawQuery == zoneListQuery {
-						body = zoneListPageTwo
-					} else {
-						t.Fatalf("unexpected query: %s", req.URL.RawQuery)
-					}
+	const zoneListPath = "/zones"
+	const zoneListQuery = "?after=coolzone.cool&limit=2"
+	var zoneListLinkHeader = fmt.Sprintf(`</zones?%s>; rel="next"`, zoneListQuery)
+	const zoneListPageOne = `[
+    {"zone": "example.com"},
+    {"zone": "coolzone.cool"}
+  ]`
+	const zoneListPageTwo = `[
+    {"zone": "example.org"},
+    {"zone": "example.io"}
+  ]`
 
-				case zoneGetPath:
-					if req.URL.RawQuery == "" {
-						res.Header().Set("Link", zoneGetLinkHeader)
-						body = zoneGetPageOne
-					} else if req.URL.RawQuery == zoneGetQuery {
-						body = zoneGetPageTwo
-					} else {
-						t.Fatalf("unexpected query: %s", req.URL.RawQuery)
-					}
-
-				default:
-					t.Fatalf("should not hit default case")
-			}
-
-			res.WriteHeader(200)
-			res.Write([]byte(body))
-		}),
+	const zoneGetPath = "/zones/coolzone.cool"
+	const zoneGetQuery = "after=b.coolzone.cool&limit=2"
+	var zoneGetLinkHeader = fmt.Sprintf(
+		`</zones/coolzone.cool?%s>; rel="next"`, zoneGetQuery,
 	)
+	const zoneGetPageOne = `{
+    "zone": "coolzone.cool",
+    "records": [{
+      "domain":"a.coolzone.cool",
+      "short_answers": ["1.1.1.1"],
+      "type":"A"
+    },
+    {
+      "domain":"b.coolzone.cool",
+      "short_answers": ["2.2.2.2"],
+      "type":"A"
+    }]
+  }`
+	const zoneGetPageTwo = `{
+    "zone": "coolzone.cool",
+    "records": [{
+      "domain":"c.coolzone.cool",
+      "short_answers": ["3.3.3.3"],
+      "type":"A"
+    },
+    {
+      "domain":"d.coolzone.cool",
+      "short_answers": ["4.4.4.4"],
+      "type":"A"
+    }]
+  }`
+
+	handler := func(res http.ResponseWriter, req *http.Request) {
+		var body string
+		switch path := req.URL.Path; path {
+
+			case zoneListPath:
+				if req.URL.RawQuery == "" {
+					res.Header().Set("Link", zoneListLinkHeader)
+					body = zoneListPageOne
+				} else if req.URL.RawQuery == zoneListQuery {
+					body = zoneListPageTwo
+				} else {
+					t.Fatalf("unexpected query: %s", req.URL.RawQuery)
+				}
+
+			case zoneGetPath:
+				if req.URL.RawQuery == "" {
+					res.Header().Set("Link", zoneGetLinkHeader)
+					body = zoneGetPageOne
+				} else if req.URL.RawQuery == zoneGetQuery {
+					body = zoneGetPageTwo
+				} else {
+					t.Fatalf("unexpected query: %s", req.URL.RawQuery)
+				}
+
+			default:
+				t.Fatalf("should not hit default case")
+
+		}
+		res.WriteHeader(200)
+		res.Write([]byte(body))
+	}
+
+	return httptest.NewServer(http.HandlerFunc(handler))
 }
