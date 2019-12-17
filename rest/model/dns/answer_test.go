a553244ca69bcda9c1053ebb320dec748787b251
Zach Johnson <zachjohnsondev@gmail.com>
2019-12-17 11:28:49 -0500

    fix test case

--- rest/model/dns/answer_test.go
+++ rest/model/dns/answer_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"gopkg.in/ns1/ns1-go.v2/rest/model/data"
 )
 
@@ -96,7 +95,8 @@ func TestPrepareURLFWDAnswer(t *testing.T) {
 				return
 			}
 
-			require.Equal(t, tt.out, got.(*Alias))
+			assert.NoError(t, err)
+			assert.Equal(t, tt.out, got.(*Alias))
 		})
 	}
 }
