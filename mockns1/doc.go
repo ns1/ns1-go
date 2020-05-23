3e822bab7c90953fe84caee9002dece08cfa9537
gnat <gnat@gnat.org>
2020-05-23 14:33:08 -0400

    Lint fix for doc

--- mockns1/doc.go
+++ mockns1/doc.go
@@ -1,4 +1,4 @@
-// This package provides utilities to run a mock service
+// Package mockns1 provides utilities to run a mock service
 // that emulates the NS1 API suitible for mock testing
 // code that relies on the gopkg.in/ns1/ns1-go.v2 pacakge
 package mockns1
