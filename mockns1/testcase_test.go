6dcc6fdb27da6fc48bf8133d594c7f5f01ae312b
gnat <gnat@gnat.org>
2020-05-23 17:47:30 -0400

    Adding ClearTestCases()

--- mockns1/testcase_test.go
+++ mockns1/testcase_test.go
@@ -1,6 +1,8 @@
 package mockns1_test
 
 import (
+	"bytes"
+	"io/ioutil"
 	"net/http"
 	"testing"
 
@@ -9,11 +11,11 @@ import (
 )
 
 func TestTestCase(t *testing.T) {
-	t.Run("AddTestCase", func(t *testing.T) {
-		mock, _, err := mockns1.New(t)
-		require.Nil(t, err)
-		defer mock.Shutdown()
+	mock, _, err := mockns1.New(t)
+	require.Nil(t, err)
+	defer mock.Shutdown()
 
+	t.Run("AddTestCase", func(t *testing.T) {
 		t.Run("Error", func(t *testing.T) {
 			t.Run("Duplicate", func(t *testing.T) {
 				require.Nil(t, mock.AddTestCase(http.MethodGet, "test/header", http.StatusOK,
@@ -29,5 +31,25 @@ func TestTestCase(t *testing.T) {
 			require.Nil(t, mock.AddTestCase(http.MethodGet, "test/case", http.StatusOK,
 				nil, nil, "", ""))
 		})
+	})
+
+	t.Run("ClearTestCases", func(t *testing.T) {
+		require.Nil(t, mock.AddTestCase(http.MethodGet, "test/clear", http.StatusOK,
+			nil, nil, "", ""))
+
+		mw := &mockWriter{buf: bytes.NewBufferString("")}
+		req := &http.Request{
+			Method:     http.MethodGet,
+			RequestURI: "/v1/test/clear",
+			Body:       ioutil.NopCloser(bytes.NewReader([]byte(""))),
+			Header:     http.Header{},
+		}
+
+		mock.ServeHTTP(mw, req)
+		require.Equal(t, http.StatusOK, mw.status, mw.buf.String())
+
+		mock.ClearTestCases()
+		mock.ServeHTTP(mw, req)
+		require.Equal(t, http.StatusNotFound, mw.status, mw.buf.String())
 	})
 }
