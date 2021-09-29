beba727322ebedbb603be5155deb5dd24ec6e47b
Thiago Uehara <tuehara@daitan.com>
2021-09-29 16:54:37 -0300

    Added get TSIG Key

--- rest/tsig_key.go
+++ rest/tsig_key.go
@@ -1,6 +1,8 @@
 package rest
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
 
 	"gopkg.in/ns1/ns1-go.v2/rest/model/dns"
@@ -27,3 +29,36 @@ func (s *TsigService) List() ([]*dns.Tsig_key, *http.Response, error) {
 
 	return tsigKeyList, resp, nil
 }
+
+// Get takes a TSIG key name and returns a single TSIG key and its basic configuration details.
+//
+// NS1 API docs: https://ns1.com/api/#getview-tsig-key-details
+func (s *TsigService) Get(name string) (*dns.Tsig_key, *http.Response, error) {
+	path := fmt.Sprintf("tsig/%s", name)
+
+	req, err := s.client.NewRequest("GET", path, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var tk dns.Tsig_key
+	var resp *http.Response
+	resp, err = s.client.Do(req, &tk)
+
+	if err != nil {
+		switch errType := err.(type) {
+		case *Error:
+			if errType.Message == fmt.Sprintf("TSIG %s. was not found", name) {
+				return nil, resp, ErrTsigKeyMissing
+			}
+		}
+		return nil, resp, err
+	}
+
+	return &tk, resp, nil
+}
+
+var (
+	// ErrTsigKeyMissing bundles GET/POST/DELETE error.
+	ErrTsigKeyMissing = errors.New("TSIG key does not exist")
+)
