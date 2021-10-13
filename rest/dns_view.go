7cf9a950951a69cb6aa655f021b963452f8b79fe
Thiago Uehara <tuehara@daitan.com>
2021-10-13 17:04:21 -0300

    Added create

--- rest/dns_view.go
+++ rest/dns_view.go
@@ -1,6 +1,8 @@
 package rest
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
 
 	"gopkg.in/ns1/ns1-go.v2/rest/model/dns"
@@ -26,3 +28,33 @@ func (s *DNSViewService) List() ([]*dns.DNSView, *http.Response, error) {
 
 	return vl, resp, nil
 }
+
+// Create takes a *dns.DNSView and creates a new DNS View.
+//
+// The given DNSView must have at least the name
+// NS1 API docs: https://ns1.com/api#putcreate-a-dns-view
+func (s *DNSViewService) Create(v *dns.DNSView) (*http.Response, error) {
+	req, err := s.client.NewRequest("PUT", fmt.Sprintf("/v1/views/%s", v.Name), v)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := s.client.Do(req, nil)
+	if err != nil {
+		switch errType := err.(type) {
+		case *Error:
+			if errType.Resp.StatusCode == 409 {
+				return nil, ErrViewExists
+			}
+		}
+
+		return resp, err
+	}
+
+	return resp, nil
+}
+
+var (
+	// ErrViewExists bundles CREATE error.
+	ErrViewExists = errors.New("DNS view already exists")
+)
