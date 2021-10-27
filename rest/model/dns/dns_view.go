1e48831c2439ab7400058cf26efed9c4e93a66e7
Thiago Uehara <tuehara@daitan.com>
2021-10-27 08:47:12 -0300

    Small fixes

--- rest/model/dns/dns_view.go
+++ rest/model/dns/dns_view.go
@@ -5,10 +5,10 @@ type DNSView struct {
 	Name        string   `json:"name,omitempty"`
 	Created_at  int      `json:"created_at,omitempty"`
 	Updated_at  int      `json:"updated_at,omitempty"`
-	Read_acls   []string `json:"read_acls,omitempty"`
-	Update_acls []string `json:"update,omitempty"`
-	Zones       []string `json:"zones,omitempty"`
-	Networks    []int    `json:"networks,omitempty"`
+	Read_acls   []string `json:"read_acls"`
+	Update_acls []string `json:"update_acls"`
+	Zones       []string `json:"zones"`
+	Networks    []int    `json:"networks"`
 	Preference  int      `json:"preference,omitempty"`
 }
 
