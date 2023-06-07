5e0d1aa51c9247cd07308da9ededcc25777a35f9
Sophie Matthews <smatthew@yelp.com>
2023-06-07 04:48:15 -0700

    Add means to get monitoring/regions

---
+++ rest/monitor_regions.go
@@ -0,0 +1,28 @@
+package rest
+
+import (
+	"net/http"
+
+	"gopkg.in/ns1/ns1-go.v2/rest/model/monitor"
+)
+
+// RegionsService handles 'monitoring/regions' endpoint.
+type RegionsService service
+
+// List returns all monitoring jobs for the account.
+//
+// NS1 API docs: https://ns1.com/api?docId=2247
+func (s *RegionsService) List() ([]*monitor.Region, *http.Response, error) {
+	req, err := s.client.NewRequest("GET", "monitoring/regions", nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	mrl := []*monitor.Region{}
+	resp, err := s.client.Do(req, &mrl)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return mrl, resp, nil
+}
