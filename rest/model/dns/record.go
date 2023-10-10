1c3aeb5952461f7a15786c551bcd48457bd39d10
ponbiki <jcandroscavage@hotmail.com>
2023-10-10 12:39:19 -0400

    cannot omit empty blocked_tags with API POST logic

--- rest/model/dns/record.go
+++ rest/model/dns/record.go
@@ -34,7 +34,7 @@ type Record struct {
 	Tags map[string]string `json:"tags,omitempty"` // Only relevant for DDI
 
 	// List of tag key names that should not inherit from the parent zone
-	BlockedTags []string `json:"blocked_tags,omitempty"` //Only relevant for DDI
+	BlockedTags []string `json:"blocked_tags"` //Only relevant for DDI
 
 	// Read-only fields
 	LocalTags []string `json:"local_tags,omitempty"` // Only relevant for DDI
