0d6fbdb3322e69a154cb17df2ccb862751b99571
Ferdinando Formica <ferdinando.formica@ie.ibm.com>
2026-02-17 13:52:42 +0000

    Fix version

--- rest/client.go
+++ rest/client.go
@@ -14,7 +14,7 @@ import (
 )
 
 const (
-	clientVersion = "2.16.1"
+	clientVersion = "2.17.1"
 
 	defaultBase                   = "https://api.nsone.net"
 	defaultEndpoint               = defaultBase + "/v1/"
